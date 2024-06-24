d0322c338ea2b0f496fb2b355c5fa2df23c42e98
geraltguru <geraltguru@gmail.com>
2024-06-24 02:01:10 +0000

    .

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:myapp/theme/themeProvider.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatefulWidget {
-const  Home({super.key});
+  const Home({super.key});
 
   @override
-  State<Home>createState() => _HomeState();
+  State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
@@ -12,7 +15,17 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      drawer: Drawer(),
+      drawer: Drawer(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        child: Center(
+          child: CupertinoSwitch(
+            value: Provider.of<Themeprovider>(context).isDark,
+            onChanged: (value)=>   
+             Provider.of<Themeprovider>(context, listen: false).toggleTheme(),
+            
+          ),
+        ),
+      ),
     );
   }
 }
