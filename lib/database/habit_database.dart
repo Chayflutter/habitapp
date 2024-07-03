1cbe19db5478ecc40312394ee62769cbbad402d0
geraltguru <geraltguru@gmail.com>
2024-07-03 01:59:44 +0000

    .

--- lib/database/habit_database.dart
+++ lib/database/habit_database.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/cupertino.dart';
 import 'package:isar/isar.dart';
+import 'package:myapp/models/app_settings.dart';
+import 'package:myapp/models/habit.dart';
 import 'package:path_provider/path_provider.dart';
 
-class Habitdatabase extends ChangeNotifier{
-  static late Isar isar ;
+class Habitdatabase extends ChangeNotifier {
+  static late Isar isar;
 
-  static Future<void> initialize() async{
-    final dir  = await getApplicationDocumentsDirectory(); 
+  static Future<void> initialize() async {
+    final dir = await getApplicationDocumentsDirectory();
+    isar =
+        await Isar.open([HabitSchema, AppSettingsSchema], directory: dir.path);
+
+    
   }
-}
 
+  Future<void> saveFirstLaunchDate()async {
+    final existingSettings = await isar.appSettings.where().findFirst();
+    if(existingSettings == null){
+      final settings = AppSettings()..firstLaunchDate = DateTime.now();
+      await isar.writeTxn(()=>isar.appSettings.put(settings));
+    }
+  }
+
+  Future<DateTime?> getFirstLaunchDate()async{
+    final settings = await isar.appSettings.where().findFirst();
+    return settings?.firstLaunchDate;
+  }
+
+
+
+}
