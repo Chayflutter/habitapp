ee32ce97b01dd005c88209727f3c2bbe039db5b1
geraltguru <geraltguru@gmail.com>
2024-07-19 02:35:25 +0000

    .

--- lib/components/habitTile.dart
+++ lib/components/habitTile.dart
@@ -5,21 +5,39 @@ class MyHabitTile extends StatelessWidget {
 
   final String text;
   final bool isCompleted;
-  const MyHabitTile({super.key, required this.isCompleted, required this.text,required this.onChanged});
+  const MyHabitTile(
+      {super.key,
+      required this.isCompleted,
+      required this.text,
+      required this.onChanged});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      decoration: BoxDecoration(
-          borderRadius: const BorderRadius.all(Radius.circular(32)),
-          color: isCompleted ? Colors.green : Colors.grey.shade200),
-      padding: const EdgeInsets.all(12),
-      child: ListTile(
-        title: Text(text, style: TextStyle(color: isCompleted? Colors.white:Colors.black),),
-        leading: Checkbox(
-          value: isCompleted,
-          onChanged: onChanged,
+    return GestureDetector(
+      onTap: () {
+        if (onChanged != null) {
+          onChanged!(!isCompleted);
+        }
+      },
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 120),
+        curve: Curves.fastOutSlowIn,
+        width: double.infinity,
+        decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(Radius.circular(32)),
+            color: isCompleted ? Colors.green : Colors.grey.shade200),
+        padding: const EdgeInsets.all(12),
+        child: ListTile(
+          title: Text(
+            text,
+            style: TextStyle(color: isCompleted ? Colors.white : Colors.black),
+          ),
+          leading: Checkbox(
+            value: isCompleted,
+            activeColor: Colors.white,
+            checkColor: Colors.black,
+            onChanged: onChanged,
+          ),
         ),
       ),
     );
